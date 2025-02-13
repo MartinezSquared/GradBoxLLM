252abeb7aad760a85390a304180b3864414bf9fd
William "Taylor" Martinez <wtmartinez@ucla.edu>
2025-02-13 11:02:20 -0800

    added text extraction

--- taylor/app.py
+++ taylor/app.py
@@ -1,6 +1,26 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def extractPdf(pdfFiles):
+    text = ""
+    # For each PDF, Read the Page, and Extract to one object
+    for pdf in pdfFiles:
+        pyPdf = PdfReader(pdf)
+        for page in pyPdf.pages:
+            text += page.extract_text()
+    return text
+
+
+            
+
 
 def main():
+## Setup
+    # Load API Keys
+    load_dotenv()
+    
+## Streamlit UI
     # Chat Header
     st.set_page_config(page_icon=":books:", page_title="GradBoxLLM")
     st.header(":books: GradBoxLLM - Your Textbook AI Assistant")
@@ -11,8 +31,17 @@ def main():
     # Uploading Documents
     with st.sidebar:
         st.subheader("Your Documents")
-        st.file_uploader("Upload Textbook and Click Process")
-        st.button("Process")
+        pdfFiles = st.file_uploader(
+            "Upload Textbook and Click Process", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                # Extract PDF text
+                extractedText = extractPdf(pdfFiles)
+                st.write(extractedText)
+                # Chunk PDF Text
+
+                #Encode Chunk into Vector Store
+                
 
 if __name__ == "__main__":
     main()
